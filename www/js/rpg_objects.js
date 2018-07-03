0d975d5097838bafacd9ce983838b8fdbf20dce9
Clayton DuBoce <themageishero@gmail.com>
2018-07-03 10:27:56 -0700

    mptest

--- www/js/rpg_objects.js
+++ www/js/rpg_objects.js
@@ -562,9 +562,9 @@ Game_Variables.prototype.value = function(variableId) {
 
 Game_Variables.prototype.setValue = function(variableId, value) {
     if (variableId > 0 && variableId < $dataSystem.variables.length) {
-        if (typeof value === 'number') {
-            value = Math.floor(value);
-        }
+        //if (typeof value === 'number') {
+            //value = Math.floor(value);
+       // }
         this._data[variableId] = value;
         this.onChange();
     }
